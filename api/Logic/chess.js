e1d878e83aeb3f2d41d5a9bf92bc53454409d6e3
Serquand <esteban.vincent@efrei.net>
2022-09-03 21:33:15 +0200

    Echec done without castling and pgn

--- api/Logic/chess.js
+++ api/Logic/chess.js
@@ -10,18 +10,85 @@ export default class Game {
         this.gameMode = gameMode
         this.idGame = idGame
         this.watchers = new Array(0)
+        this.selectedPiece = null
+        this.waitingPromotion = null
+    }
+
+    getPiece(indexClick) {
+        const allPieces = this.game.board()
+        return allPieces[parseInt(indexClick / 8)][indexClick % 8];
+    }
+
+    transformIndexInSquare(index) {
+        return String.fromCharCode(97 + index % 8) + (8 - parseInt(index / 8))
+    }
+
+    transformSquareInIndex(square) {
+        for(let i = 0; i < square.length; i++) {
+            square[i] = square[i].split("=")[0]
+            const numberCase = square[i].charAt(square[i].length - 1)
+            let letterCase = square[i].charAt(square[i].length - 2)
+            letterCase = (letterCase.charCodeAt(0) - 97)
+            square[i] = letterCase + (8 - numberCase) * 8
+        }
+        return square
+    }
+
+    movePiece(indexClick) {
+        const possibleMove = this.game.moves({ square: this.transformIndexInSquare(this.selectedPiece) })
+        for (const moves of possibleMove) {
+            if(moves.includes("=")) {
+                this.waitingPromotion = indexClick
+                return false
+            }
+        }
+
+        this.game.move({ from: this.transformIndexInSquare(this.selectedPiece), to: this.transformIndexInSquare(indexClick) })
+        this.selectedPiece = null
+        
+        return {
+            map: this.getMap(this.game.board()),
+            pgn: this.game.pgn()
+        }
+    }
+
+    getPossibleMove(indexClick) {
+        const square = this.transformIndexInSquare(indexClick)
+        return this.transformSquareInIndex(this.game.moves({ square }))
+    }
+
+    createPromotion(choice) {
+        this.game.move(this.createPromotionMove(choice))
+        this.selectedPiece = null
+        this.waitingPromotion = null
+        return { map: this.getMap(this.game.board()), pgn: this.game.pgn() }
+    }
+
+    createPromotionMove(choice) {
+        let allMoves = this.game.moves({ square: this.transformIndexInSquare(this.selectedPiece) })
+        allMoves = allMoves.filter(move => move.includes("=" + choice))
+        const oldLetter = this.transformIndexInSquare([this.selectedPiece])[0].charAt(0), 
+        newLetter = this.transformIndexInSquare([this.waitingPromotion])[0].charAt(0)
+        allMoves = allMoves.filter(move => move.startsWith(oldLetter))
+        if(oldLetter != newLetter) allMoves = allMoves.filter(move => move.includes('x' + newLetter + '8')) 
+        else allMoves = allMoves.filter(move => !(move.includes('x' + newLetter + '8')))
+        return allMoves[0]
+    }
+
+    getMap(chessboard) {
+        let tempArr = new Array(0)
+
+        for(let i = 0; i < 8; i++) {
+            for(let j = 0; j < 8; j++) {
+                if(chessboard[i][j]) tempArr.push(chessboard[i][j].color + chessboard[i][j].type.toUpperCase())
+                else tempArr.push(null)
+            }   
+        }
+
+        return tempArr
+    }
+
+    getGoodPiece(indexClick) {
+        return this.getPiece(indexClick)?.color === this.game.turn()
     }
 }
-
-// chess.move('a4')
-// chess.move('g6')
-// chess.move('a5')
-// chess.move('g5')
-// chess.move('a6')
-// chess.move('g4')
-// chess.move('axb7')
-// chess.move("g3")
-
-// console.log(chess.moves());
-
-// console.log(chess.pgn(), chess.board())
--- api/server.js
+++ api/server.js
@@ -11,10 +11,14 @@ const PORT = process.env.PORT || 6251
 
 const sessions = {}
 
+const isTheGoodClient = (socket, idSession) => socket.rooms.has((sessions[idSession].game.game.turn() === 'w' ? 'firstPlayer - ' : 'secondPlayer - ') + idSession)
+
+
 io.on("connection", socket => {
     console.log("A new user is connected !")
 
     socket.on("responseUser", msg => {
+        if(!msg?.includes("-")) return
         const user = msg.split("-")[1].trim()
         msg = msg.split("-")[0].trim()
         socket.session = msg
@@ -23,10 +27,15 @@ io.on("connection", socket => {
             sessions[msg] = [socket]
             sessions[msg].game = new Game(user, 'Simple', msg)
             socket.join("firstPlayer - " + msg)
+            socket.join("players - " + msg)
+            socket.emit("init")
         } else {
             if(sessions[msg].length == 1) {
+                socket.join("players - " + msg)
                 socket.join("secondPlayer - " + msg)
                 sessions[msg].game.secondPlayer = user
+                io.to(["players - " + msg]).emit("play")
+                
                 console.log("Nous allons lancer un match opposant " + 
                     sessions[msg].game.firstPlayer + " à " + sessions[msg].game.secondPlayer
                 );
@@ -37,6 +46,27 @@ io.on("connection", socket => {
             }
             sessions[msg].push(socket)
         }
+
+        socket.on("newInfoClick", indexClick => {
+            if(isTheGoodClient(socket, msg)) {
+                if(sessions[msg].game.selectedPiece) {
+                    const infoMove = sessions[msg].game.movePiece(indexClick)
+                    if(typeof(infoMove) === 'object') io.to(["players - " + msg, "Watchers " + msg]).emit("moveTo", infoMove.map, infoMove.pgn)
+                    else socket.emit("promotion", sessions[msg].game.game.turn())
+                } else if(sessions[msg].game.getGoodPiece(indexClick)) {
+                    sessions[msg].game.selectedPiece = indexClick
+                    const possibleMove = sessions[msg].game.getPossibleMove(indexClick)
+                    if(possibleMove.length == 0) sessions[msg].game.selectedPiece = null 
+                    else socket.emit("showMoveTab", possibleMove)
+                } 
+            }
+        })
+
+        socket.on("promotionChoice", choice => {
+            console.log(choice)
+            const infoMove = sessions[msg].game.createPromotion(choice)
+            io.to(["players - " + msg, "Watchers " + msg]).emit("moveTo", infoMove.map, infoMove.pgn)
+        })
     })
 })
 

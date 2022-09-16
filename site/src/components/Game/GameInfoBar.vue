0a198762ee19c7b115675d442ff851d84526f8b0
Serquand <esteban.vincent@efrei.net>
2022-09-16 23:36:32 +0200

    Add giveUp in GameInfoBar

---
+++ site/src/components/Game/GameInfoBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <div 
+            @click="sendGiveUp"
+            class="give-up-container"
+        >
+            <img src="/assets/GamePanel/giveup.png" alt="giveUp" />
+        </div>
+        <div class="draw-proposal"></div>
+        <div class="viewer-count-container"></div>
+    </section>
+</template>
+
+<script>
+export default {
+    methods: {
+        sendGiveUp() {
+            this.$emit("giveUp")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

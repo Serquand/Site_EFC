7811620abe36c536fbc56eb16cbbbe18837793f4
Serquand <esteban.vincent@efrei.net>
2022-09-18 20:54:32 +0200

    Setup front fetch

---
+++ site/src/views/Review.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Review</h1>
+</template>
+
+<script>
+import { url } from '../../config.json'
+import { ref } from 'vue'
+
+
+export default {
+    setup() {
+        const pgn = ref(""), firstPlayer = ref(""), secondPlayer = ref(""), firstElo = ref(0), secondElo = ref(0)
+        return { pgn, firstPlayer, secondPlayer, firstElo, secondElo }
+    }, 
+
+    async created() {
+        const res = await (await fetch(url + "/review/" + this.$route.params.idGame)).json()
+        console.log(res)
+    }
+}
+</script>
+
+<style>
+
+</style>

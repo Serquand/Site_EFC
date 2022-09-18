b86f12b17357d82c5d23366c7556517c127b55bb
Serquand <esteban.vincent@efrei.net>
2022-09-18 22:38:23 +0200

    Updated PGN on ReviewGame mdw for array

--- api/Logic/Middleware/Profil/ReviewGame.js
+++ api/Logic/Middleware/Profil/ReviewGame.js
@@ -1,6 +1,19 @@
 import Games from "../../../Models/Games.js";
 import { pseudoWhoIdBelongs, resultToLetter } from "./GetProfil.js";
 
+const convertPgnToArray = pgn => {
+    let movesTab = new Array(0)
+    const temp = pgn.split(".")
+    temp.shift()
+    for(let i = 0; i < temp.length; i++) {
+        temp[i] = temp[i].trim()
+        const moveExchange = temp[i].split(" ")
+        movesTab.push(moveExchange[0])
+        movesTab.push(moveExchange[1])
+    }
+    return movesTab
+}
+
 export default async function review(req, res) {
     let reviewGame = await Games.findOne({
         where: { id: req.params.idGame }, 
@@ -9,6 +22,7 @@ export default async function review(req, res) {
 
     reviewGame.player1 = await pseudoWhoIdBelongs(reviewGame.player1)
     reviewGame.player2 = await pseudoWhoIdBelongs(reviewGame.player2)
+    reviewGame.pgn = convertPgnToArray(reviewGame.pgn)
     reviewGame = reviewGame.dataValues
 
     res.status(200).json({ reviewGame })

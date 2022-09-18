5f3978f4be090c5ad233392602936e97c9912f25
Serquand <esteban.vincent@efrei.net>
2022-09-18 08:56:47 +0200

    Setup GameProfil component

---
+++ site/src/components/Profil/GameProfil.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="game-profil-container">
+        <div class="player-container">
+            <p>{{ game.player1 }}</p>
+            <p>{{ '(' + game.eloPlayer1 + ')' }}</p>
+            <p>{{ game.player2 }}</p>
+            <p>{{ '(' + game.eloPlayer2 + ')' }}</p>
+        </div>
+
+        <div class="result-container">
+            <div class="result">
+                <p>{{ game.result }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup(props) {
+        const thisGame = JSON.parse(JSON.stringify(props.game))
+        return { thisGame }
+    },  
+
+    props: {
+        game: {
+            type: Object, 
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- site/src/views/Profil.vue
+++ site/src/views/Profil.vue
@@ -1,21 +1,44 @@
 <template>
-    <h1>Profil</h1>
+    <div class="profil-container">
+        <div class="info-profil-container">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div class="games-container">
+            <div 
+                class="game-content"
+                v-for="game in games"
+                :key="game"
+            >
+                <GameProfil :game="game" />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import { url } from '../../config.json' 
 import { ref } from 'vue'
+import GameProfil from '../components/Profil/GameProfil.vue'
 
 export default {
     setup() {
-        const games = ref([]), profil = ref({})
-
-        return { games, profil }
-    }, 
+        const games = ref([]), profil = ref({});
+        return { games, profil };
+    },
 
     async created() {
-        const profil = await (await fetch(url + "/profil/profil/" + this.$route.params.searchedUser)).json()
-        console.log(profil)
+        const profil = await (await fetch(url + "/profil/profil/" + this.$route.params.searchedUser)).json();
+        console.log(profil);
+        this.games = profil.allGames
+        this.profil = profil.profil
+
+    },
+    
+    components: { 
+        GameProfil 
     }
 }
 </script>
